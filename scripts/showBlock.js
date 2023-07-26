77700996a779daa3f22eb2a32497941a7c67f2ef
Stef Lund <stef@Stephanies-Air.fritz.box>
2023-07-26 16:18:08 +0200

    Make loading bar smoother

--- scripts/showBlock.js
+++ scripts/showBlock.js
@@ -1,8 +1,9 @@
 const favicon = "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAJkSURBVHgB7VZBbtpQEH3zIW0WVYuXVaH4Bs0NSk4AOUFhEarskhMknIDsqkKlcIT0BNAT1D1B3ZJK3dmVuirwp/MhVmzAxiagKBJv9+ePZ97M/JkxsMMODwzChlD84FWQp3MxeCDHAhiumB+MJrr1+8Ryw3p/9+H4DctfIPCq49Xlw8Kv99YlMuB19885gy/i7llziwGfFFWJyR02XzSCuwiBUse7BlFVaz5LS8KQVkRXaXRJsqImfDjKSZBNyzEyFWFKVJ4KFbWLElUao6KbSk8i9TXgTPaorxTskPwOxa7/9baGt4zg8oQbNyfWYJlRU0/KUx9ZwNwYNq1ecFRzl18QpW0bB0Ks//KjV1uwlbuLJA3GxEdh5wb5yGEPl3qMd2xecYQHKnlFlVLX95kxYCFKGg5IlU2a0uLpCM68LEJA+sJ/Dm6Jy3aMjQIRakRUm+UuvfOp/X34iQSejeFo0Hdx4optG5uFH/R+GHNvANcm3VtwLs+Lvy2TRwhIOnrYHhysIuDKcCDwGbYAjglOzQt+HssElF6dvoNNOZeuCSbfSgIGMjILMo4/ExZf7TqghNLmlwm1gpSC2tmaLAZMvWGz0Iu7XpqBm2NrQNN5cD+Y5ZOTdZyok3RZMusZOJUN+QZrQFb0oQkG6xIIYHe8A03Unx/Ryd6jS2ctAsbxmFRVynGKlM5na5ePVkUe0p+h9MmraS2zXqYgmSWjOPtElHbLTVB3Q79gqQlMScxqXpeav0UWiGMmXKSNOpZAAPvKs/U/1MRoxRxl+5WD+psUy2D5IdmRVoWjnqDnLlkyO+zwaPAf1zXwZL751PUAAAAASUVORK5CYII=";
 var qwitterBypassed = false;
 
+const loading_bar_width = 300;
 const loading_time = 1000 * 10;
-const increment = loading_time / 100;
+const increment = loading_time / loading_bar_width;
 
 
 // cover whole page with qwitter loading screen
@@ -53,7 +54,7 @@ function startTimer(quotes) {
 
 // increment the progress bar 1% at a time
 function moveBar(loading, i) {
-    if (i == 100) {
+    if (i == loading_bar_width) {
         // remove bar and add buttons after bar is done
         loading.parentElement.remove();
         addButtons();
@@ -61,7 +62,7 @@ function moveBar(loading, i) {
     else {
         i++;
         window.scrollTo(0, 0);
-        loading.style.width = i + "%";
+        loading.style.width = i + "px";
         setTimeout(moveBar.bind(null, loading, i), increment);
     }
 }
